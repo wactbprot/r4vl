1bad65807954bb61b0cdae577dfa27b85b2f08c9
wactbprot <thsteinbock@web.de>
2011-06-22 13:45:06 +0200

    new file:   _attachments/scripts/analyse_CE3_KK.R

---
+++ _attachments/scripts/analyse_CE3_KK.R
@@ -0,0 +1,12 @@
+if(length(doc$Calibration) > 0){
+doc <- refreshAnalysis(cdb,doc)
+doc <- getOutIndex(doc)
+doc <- ce3.calT(doc)
+doc <- ce3.calPfill(doc)
+doc <- ce3.calDvC(doc)
+doc <- ce3.calQ(doc)
+doc <- ce3.calMfp(doc)
+doc <- ce3.writePind(doc)
+doc <- ce3.calPcal(doc)
+
+}
