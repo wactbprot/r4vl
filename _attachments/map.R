9c0a97a70c0cacc1d65be6610474361d70aadaf6
wactbprot <thsteinbock@web.de>
2012-08-14 11:54:13 +0200

    clean up ii

---
+++ _attachments/map.R
@@ -0,0 +1,8 @@
+## --
+## wactbprot/2012-08-14
+## --
+library(methods,  quietly =TRUE)
+library(bitops,   quietly =TRUE)
+library(RJSONIO,  quietly =TRUE)
+library(RCurl,    quietly =TRUE)
+library(R4CouchDB,quietly =TRUE) 
