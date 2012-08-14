cf10514c004011384f31f3896c00bd481c2949a2
wactbprot <thsteinbock@web.de>
2012-08-14 15:01:59 +0200

    map.R

--- _attachments/map.R
+++ _attachments/map.R
@@ -6,3 +6,38 @@ library(bitops,   quietly =TRUE)
 library(RJSONIO,  quietly =TRUE)
 library(RCurl,    quietly =TRUE)
 library(R4CouchDB,quietly =TRUE) 
+
+infList          <- list()
+
+infList$args     <- commandArgs(TRUE) 
+
+infList$srcPath  <- infList$args[1]
+
+setwd(infList$srcPath)
+
+infList$utilsPath     <- "./R/utils/"
+infList$calPath       <- "./R/cal/"
+infList$uncertPath    <- "./R/uncert/"
+
+infList$scriptPath    <- "./scripts/"
+
+infList$srcPat   <- "R$"
+
+
+fn <- list.files(infList$utilsPath, pattern=infList$srcPat)
+for (k in 1:length(fn)){
+  source(paste(infList$utilsPath,fn[k],sep=""))
+}
+
+fn <- list.files(infList$calPath, pattern=infList$srcPat)
+for (k in 1:length(fn)){
+  source(paste(infList$calPath,fn[k],sep=""))
+}
+
+fn <- list.files(infList$uncertPath, pattern=infList$srcPat)
+for (k in 1:length(fn)){
+  source(paste(infList$uncertPath,fn[k],sep=""))
+}
+
+
+toJSON(infList)
