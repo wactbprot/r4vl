5284a46613348d61b562c75a21dbdbc007cdad79
wactbprot <thsteinbock@web.de>
2013-09-06 10:19:08 +0200

    Reporterzeugung durch Task startbar

--- chunks/ce3.conductance.R
+++ chunks/ce3.conductance.R
@@ -5,72 +5,65 @@ sdcnom         <- getConstVal(a$cav$Conductance, "sd_cnom")
 cfm3           <- getConstVal(a$cav$Conductance, "cfm3")
 diffhist       <- getConstVal(a$cav$Conductance, "diff_hist")
 
-
-
-
-iout <- which(abs(diffhist) > 0.1) # alles was mehr als 10% abweicht ist Schrott
+iout <- which((abs(diffhist) > 0.5) | cnom < 1e-8 | is.na(cfm3)) 
 if(length(iout) > 0){
-cnom           <-  cnom[-iout]       
-sdcnom         <-  sdcnom[-iout]    
-cfm3           <-  cfm3[-iout]      
-diffhist       <-  diffhist[-iout]  
+    cnom           <-  cnom[-iout]
+    sdcnom         <-  sdcnom[-iout]
+    cfm3           <-  cfm3[-iout]
+    diffhist       <-  diffhist[-iout]
 
-
-
-
-if(length( a$cav$Uncertainty) > 0){
-    withU     <- TRUE
-    uDV       <- getConstVal(a$cav$Uncertainty, "uncertDeltaV")[-iout]
-    uDVDt     <- getConstVal(a$cav$Uncertainty, "uncertDeltaVDeltat")[-iout]
-    uDt       <- getConstVal(a$cav$Uncertainty, "uncertDeltat")[-iout]
-}
+    if(length( a$cav$Uncertainty) > 0){
+        withU     <- TRUE
+        uDV       <- getConstVal(a$cav$Uncertainty, "uncertDeltaV")[-iout]
+        uDVDt     <- getConstVal(a$cav$Uncertainty, "uncertDeltaVDeltat")[-iout]
+        uDt       <- getConstVal(a$cav$Uncertainty, "uncertDeltat")[-iout]
+    }
 
 }else{
 
 
-if(length( a$cav$Uncertainty) > 0){
-    withU     <- TRUE
-    uDV       <- getConstVal(a$cav$Uncertainty, "uncertDeltaV")
-    uDVDt     <- getConstVal(a$cav$Uncertainty, "uncertDeltaVDeltat")
-    uDt       <- getConstVal(a$cav$Uncertainty, "uncertDeltat")
-}
+    if(length( a$cav$Uncertainty) > 0){
+        withU     <- TRUE
+        uDV       <- getConstVal(a$cav$Uncertainty, "uncertDeltaV")
+        uDVDt     <- getConstVal(a$cav$Uncertainty, "uncertDeltaVDeltat")
+        uDt       <- getConstVal(a$cav$Uncertainty, "uncertDeltat")
+    }
 }
 
 N <- length(cnom)
 
-
 if(withU){
     df    <- data.frame(
-        cnom      =   cnom,     
-        sdcnom    =   sdcnom ,  
-        cfm3      =   cfm3,     
-        diffhist  =   diffhist, 
-        uDV       =  uDV,    
-        uDVDt     =  uDVDt,  
-        uDt       =  uDt,    
+        cnom      =   cnom,
+        sdcnom    =   sdcnom ,
+        cfm3      =   cfm3,
+        diffhist  =   diffhist,
+        uDV       =  uDV,
+        uDVDt     =  uDVDt,
+        uDt       =  uDt,
         Mpkt       = 1:N)
-    
+
     ndf <- melt(df, id=c("Mpkt","uDV","uDVDt","uDt","diffhist","sdcnom"))
-    
+
 }else{
     df    <- data.frame(
-        cnom      =   cnom,     
-        sdcnom    =   sdcnom,   
-        cfm3      =   cfm3,     
-        diffhist  =   diffhist, 
+        cnom      =   cnom,
+        sdcnom    =   sdcnom,
+        cfm3      =   cfm3,
+        diffhist  =   diffhist,
         Mpkt      = 1:N)
-    
+
     ndf <- melt(df, id=c("Mpkt","diffhist","sdcnom"))
-  
-   
+
+
 }
-N             <- length(pcal)
+
 
 pltc <- ggplot(ndf)
 pltc <- pltc + geom_point(aes(x = Mpkt,
-                                    y = value,
-                                    color = factor(ndf$variable)),
-                                size=5)
+                              y = value,
+                              color = factor(ndf$variable)),
+                          size=5)
 pltc <- pltc +    guides(color = guide_legend("Conductance"))
 pltc <- pltc +    scale_y_log10()
 pltc <- pltc +    theme(legend.position="bottom")
@@ -82,5 +75,5 @@ if(withU){
     pltc <- pltc +  geom_errorbar(aes(x=Mpkt,
                                       ymax = cnom*(1 + (uDV^2 +uDt^2 +uDVDt^2)^.5 ),
                                       ymin = cnom*(1 - (uDV^2 +uDt^2 +uDVDt^2)^.5)), col=2)
-   
+
 }
--- genR.R
+++ genR.R
@@ -6,10 +6,12 @@ infList             <- list()
 infList$args        <- commandArgs(TRUE) 
 noOfArgs            <- length(infList$args)
 
+instPath            <- "/usr/local/lib/r4vl/"
 templatePath        <- "templates/"
+figurePath          <- "figures/"
 tmpPath             <- "/tmp/"
 
-setwd("/usr/local/lib/r4vl")
+setwd(instPath)
  
 infList$callScript  <- infList$args[1]
 
@@ -26,15 +28,23 @@ doc                 <- cdbGetDoc(cdb)$res
 a                   <- abbrevList(doc)
 reportName          <- paste(a$cy,a$ct,a$cs,a$csi, sep="-")
 outPath             <- paste(tmpPath, reportName,"/", sep="")
-dir.create(outPath, showWarnings = FALSE, mode = "0777")
- 
+
+dir.create(outPath,
+           showWarnings = FALSE,
+           mode = "0777")
+setwd(outPath)
+dir.create(paste(outPath,
+                 figurePath , sep=""),
+           showWarnings = FALSE,
+           mode = "0777")
+
  ## generate report
-opts_chunk$set(fig.width=8, fig.height=6, fig.path=outPath)
+opts_chunk$set(fig.width=8, fig.height=6, fig.path=figurePath)
 options( digits = 8)
 
 if(a$cs == "CE3"){
-    knit(paste(templatePath, "ce3.report.Rhtml", sep=""),
-         paste(outPath ,reportName, ".html", sep=""))
+    knit(paste(instPath, templatePath, "ce3.report.Rhtml", sep=""),
+         paste(reportName, ".html", sep=""))
 }
 
  ## build new doc
@@ -45,19 +55,19 @@ if(a$cs == "CE3"){
  ndoc$Type     <-a$ct
  ndoc$Year     <-a$cy
  
- outdb         <- cdbIni() 
+outdb         <- cdbIni() 
 outdb$DBName  <- "vaclab_ext"
 outdb$id      <- cdb$id
 
- outdb$dataList <- ndoc
+outdb$dataList <- ndoc
 try(cdbUpdateDoc(outdb)$res, TRUE)
 
-figures <- list.files(outPath, pattern="png$")
+figures <- list.files(figurePath, pattern="png$")
  
 for( fig in figures){
-    outdb$fileName <-  paste(outPath, fig, sep="")
+    outdb$fileName <-  paste(figurePath, fig, sep="")
     tmp            <- cdbAddAttachment(outdb)$res
 }
 
-outdb$fileName     <-  paste(outPath,reportName, ".html", sep="")
+outdb$fileName     <-  paste(reportName, ".html", sep="")
 tmp                <- cdbAddAttachment(outdb)$res
