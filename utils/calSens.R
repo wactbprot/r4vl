8a41c007e4dd9a8615275d36e7597c54f79ffe32
wactbprot <thsteinbock@web.de>
2013-09-11 16:16:48 +0200

    utils

--- utils/calSens.R
+++ utils/calSens.R
@@ -1,18 +1,22 @@
 calSens <- function( ccc ){
+  
+  msg   <- "calculated by calSens()"
+  a     <- abbrevList(ccc)
+  iUnit <- "A"
 
-  msg <- "calculated by calSens()"
-  a   <- abbrevList(ccc)
-
-  ie   <- 0.305e-3
-  ioff <- getConstVal(a$cmv, "offset")
-  iind <- getConstVal(a$cmv, "ind")
-  pcal <- getConstVal(a$cav, "cal")
+  IE    <- getSubList(a$cmco1, "ie")
+  iconv <- getConvFactor(ccc,iUnit, IE$Unit)
+  ie    <- getConstVal(NA, NA, IE) * iconv
+  
+  ioff  <- getConstVal(a$cmv, "offset")
+  iind  <- getConstVal(a$cmv, "ind")
+  pcal  <- getConstVal(a$cav, "cal")
 
     ccc$Calibration$Analysis$Values$Error<-
       setCcl(ccc$Calibration$Analysis$Values$Error, "relative",
              "1",
              (iind - ioff)/(pcal * ie) ,
-             msg)
+             paste(msg, "Ie is given in 1st calib. obj. with: ", ie,IE$Unit))
   
   return( ccc )
 }
