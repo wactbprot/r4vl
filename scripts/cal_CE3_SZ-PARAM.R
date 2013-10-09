8bc19a6e908be068db937c681a292c5022f7a59f
wactbprot <thsteinbock@web.de>
2013-10-09 14:51:40 +0200

    doku ce3-sz-param

--- scripts/cal_CE3_SZ-PARAM.R
+++ scripts/cal_CE3_SZ-PARAM.R
@@ -1,7 +1,6 @@
 #'
-#' Function calculates the CE3 saw tooth params which will be written
-#' to the provided textboxes by YAMP.
-#'
+#' Funktion zur Berechnung des Fülldrucks und der Sägezahnparameter 
+#' 
 #' @author wactbprot (thsteinbock@web.de)
 #' @export
 #' @keywords yamp
@@ -10,63 +9,69 @@
 pcal    <- as.numeric(infList$args[2])
 a       <-  abbrevList(doc)
 
-
 ## andere Gase kommen noch
 if((a$cmscg == "N2" || a$cmscg == "Ar" || a$cmscg == "D2") & is.numeric(pcal)){
-    
-  gas   <- a$cmscg
-  cf    <- list()
 
-  if(pcal < 9e-7){
-      lw <- "lw0"
-  }else{
-      lw <- "lw1"
-  }
+    gas   <- a$cmscg
+    cf    <- list()
 
-  NOMC1 <- getSubList(a$cms,"nomC1")
-  
-  conv <- getConvFactor(doc, "l/s" , NOMC1$Unit)
-                             #^^to , ^^from
-  Cp <- getConstVal(NA, NA, NOMC1) * conv # f. l/s
+    if(pcal < 9e-7){
+        lw <- "lw0"
+    }else{
+        lw <- "lw1"
+    }
 
-  
-  if(lw == "lw0" ){ ## kl.LW
-    cf$a   <-  getConstVal(a$cms, paste("klLw_",gas,"_A", sep=""))
-    cf$b   <-  getConstVal(a$cms, paste("klLw_",gas,"_B", sep=""))
-    cf$c   <-  getConstVal(a$cms, paste("klLw_",gas,"_C", sep=""))
-    cf$d   <-  getConstVal(a$cms, paste("klLw_",gas,"_D", sep=""))
-    E      <- 0.00095 ## 
-    noMp   <- 100
-    V      <- 0.07673 ## l aus AA
-    tc     <- 1.8e-3  ## call offset 2ms
-    pfill  <- 0.26    ## start pfill
-  }
+    NOMC1 <- getSubList(a$cms,"nomC1")
+    conv <- getConvFactor(doc, "l/s" , NOMC1$Unit)
+                                        #^^to , ^^from
+    Cp <- getConstVal(NA, NA, NOMC1) * conv # f. l/s
 
-  if(lw == "lw1" ){ ## gr.LW
-    cf$a  <-  getConstVal(a$cms, paste("grLw_",gas,"_A", sep=""))
-    cf$b  <-  getConstVal(a$cms, paste("grLw_",gas,"_B", sep=""))
-    cf$c  <-  getConstVal(a$cms, paste("grLw_",gas,"_C", sep=""))
-    cf$d  <-  getConstVal(a$cms, paste("grLw_",gas,"_D", sep=""))
-    E     <- 0.0015 ## war 0.001 (mehr zeit bei 350 mbar) 
-    noMp  <- 90
-    V     <- 0.2808 ## l aus AA
-    tc    <- 2.1e-3  ## call offset 2ms
-    pfill <- 8.1    ## start pfill
-  }
 
-  pcal.pred <- fn.2162(cf,pfill)/Cp * pfill
-  for(i in 1:10){
+    if(lw == "lw0" ){
+        ## Parameter
+        ## kleiner Leitwert
+        ##
+        cf$a   <-  getConstVal(a$cms, paste("klLw_",gas,"_A", sep=""))
+        cf$b   <-  getConstVal(a$cms, paste("klLw_",gas,"_B", sep=""))
+        cf$c   <-  getConstVal(a$cms, paste("klLw_",gas,"_C", sep=""))
+        cf$d   <-  getConstVal(a$cms, paste("klLw_",gas,"_D", sep=""))
+        E      <- 0.00095 ##
+        noMp   <- 100     ## Anzahl der Messpunkte
+        V      <- 0.07673 ## Volumen in l aus AA
+        tc     <- 1.8e-3  ## call offset 2ms
+        pfill  <- 0.26    ## start pfill
+    }
 
-      e         <- pcal.pred/pcal - 1
-      pfill     <- pfill * (1 - e)
-      pcal.pred <- fn.2162(cf,pfill)/Cp * pfill
+    if(lw == "lw1" ){
+        ## Parameter 
+        ## großer Leitwert
+        ##
+        
+        cf$a  <-  getConstVal(a$cms, paste("grLw_",gas,"_A", sep=""))
+        cf$b  <-  getConstVal(a$cms, paste("grLw_",gas,"_B", sep=""))
+        cf$c  <-  getConstVal(a$cms, paste("grLw_",gas,"_C", sep=""))
+        cf$d  <-  getConstVal(a$cms, paste("grLw_",gas,"_D", sep=""))
+        E     <- 0.0015 ## 
+        noMp  <- 90     ## Anzahl der Messpunkte
+        V     <- 0.2808 ## Volumen in l aus AA
+        tc    <- 2.1e-3 ## call-offset 2ms
+        pfill <- 8.1    ## start pfill
+    }
+    ##
+    ## Berechnen des pfill aus der Leitwertkurve
+    ## und der Differenz zum Zieldruck
+    ##
+    pcal.pred     <- fn.2162(cf, pfill)/Cp * pfill
+    for(i in 1:10){
+        e         <- pcal.pred/pcal - 1
+        pfill     <- pfill * (1 - e)
+        pcal.pred <- fn.2162(cf, pfill)/Cp * pfill
 
-  }
+    }
 
-  
-  T       <-  V/fn.2162(cf,pfill) * E - noMp * tc ## gesamtmesszeit pro SZ
-  tm      <- (T/noMp) * 1000
-  
-  cat(toJSON(list("target_p_fill" = pfill, "lwx" = lw,"sz_time"=tm, "mp_repeat"= noMp  )))
+    T       <-  V / fn.2162(cf, pfill) * E - noMp * tc ## Gesamtmesszeit pro SZ
+    tm      <- (T / noMp) * 1000
+
+    cat(toJSON(list("target_p_fill" = pfill, "lwx" = lw,"sz_time"=tm, "mp_repeat"= noMp  )))
 
 }
