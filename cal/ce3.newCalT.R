d9289a16b2adb37008906923bc48f1f16ad51edb
wactbprot <thsteinbock@web.de>
2013-10-18 10:40:48 +0200

    start opk4

--- cal/ce3.newCalT.R
+++ cal/ce3.newCalT.R
@@ -1,53 +1,62 @@
 ce3.newCalT <- function(ccc){
 
-  msg <- "calculated by ce3.newCalT"
+    msg <- "calculated by ce3.newCalT"
+    a   <- abbrevList(ccc)
 
-  a   <- abbrevList(ccc)
-  N   <- length(getConstVal(a$cm,"drift_slope_x"))
-  C2K <- getConstVal(a$cc, "C_2_K")
+    C2K <- getConstVal(a$cc, "C_2_K")
 
-  ## T_ptb - Tch als Korrektur benutzt wird, muss
-  ## Tch + K gerechnet werden: Tch + K =   Tch + (T_ptb - Tch) = T_ptb
 
-  baseCorrName <- "agilentCorrCh"
-  baseNameVal <- "agilentCh"
-  
-  chn   <- c("110")
-  tRoom <-  getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
-  
-  chn   <- c("103")
-  tpbox <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
-  
-  chn   <- c("104","105","106","107")
-  tUhv <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
 
-  chn   <- c("108","109")
 
-  tXhv <-  getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
+    N   <- length(getConstVal(a$cm,"drift_slope_x"))
 
-  sufNameVal  <- "_after_lw"
-  chn         <- c("101","102")
-  tFma        <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName,sufNameVal)
+    ## T_ptb - Tch als Korrektur benutzt wird, muss
+    ## Tch + K gerechnet werden: Tch + K =   Tch + (T_ptb - Tch) = T_ptb
 
-  sufNameVal  <- "_before_lw"
-  chn         <- c("101","102")
-  tFmb        <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName,sufNameVal)
+    baseCorrName <- "agilentCorrCh"
+    baseNameVal <- "agilentCh"
+    chn   <- c("110")
+    tRoom <-  getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
 
-  tFm <- (tFma+tFmb)/2
-  
-  ccc$Calibration$Analysis$Values$Temperature <-
-    setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tfm3","K",tFm + C2K,msg)
-  ccc$Calibration$Analysis$Values$Temperature <-
-    setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tuhv","K",tUhv + C2K,msg)
-  ccc$Calibration$Analysis$Values$Temperature <-
-    setCcl( ccc$Calibration$Analysis$Values$Temperature,"Txhv","K",tXhv + C2K,msg)
-  ccc$Calibration$Analysis$Values$Temperature <-
-    setCcl( ccc$Calibration$Analysis$Values$Temperature,"Troom","K",tRoom + C2K,msg)
-  ccc$Calibration$Analysis$Values$Temperature <-
-    setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tpbox","K",tpbox + C2K,msg)
+    chn   <- c("103")
+    tpbox <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
 
-  return(ccc)
+    chn   <- c("104","105","106","107")
+    tUhv <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
 
+    chn   <- c("108","109")
+
+    tXhv <-  getTemperatureVec(a,N,chn,baseNameVal,baseCorrName)
+
+    if(a$cmscok == "opK1" |a$cmscok == "opK2"|a$cmscok == "opK3"){
+
+        sufNameVal  <- "_after_lw"
+        chn         <- c("101","102")
+        tFma        <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName,sufNameVal)
+
+        sufNameVal  <- "_before_lw"
+        chn         <- c("101","102")
+        tFmb        <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName,sufNameVal)
+
+        tFm <- (tFma+tFmb)/2
+    }
+
+    if(a$cmscok == "opK4"){
+        chn         <- c("101","102")
+        tFm        <- getTemperatureVec(a,N,chn,baseNameVal,baseCorrName,sufNameVal)
+    }        
+
+    ccc$Calibration$Analysis$Values$Temperature <-
+        setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tfm3","K",tFm + C2K,msg)
+    ccc$Calibration$Analysis$Values$Temperature <-
+        setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tuhv","K",tUhv + C2K,msg)
+    ccc$Calibration$Analysis$Values$Temperature <-
+        setCcl( ccc$Calibration$Analysis$Values$Temperature,"Txhv","K",tXhv + C2K,msg)
+    ccc$Calibration$Analysis$Values$Temperature <-
+            setCcl( ccc$Calibration$Analysis$Values$Temperature,"Troom","K",tRoom + C2K,msg)
+    ccc$Calibration$Analysis$Values$Temperature <-
+        setCcl( ccc$Calibration$Analysis$Values$Temperature,"Tpbox","K",tpbox + C2K,msg)
+    
+    return(ccc)
+    
 }
-
-
