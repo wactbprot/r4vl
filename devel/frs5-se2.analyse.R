db1d439aedf9a55f4406c84c6500048cc3396262
Thomas Bock <thsteinbock@web.de>
2014-02-11 09:46:32 +0100

    start auswertung frs-se2 messung 	deleted:    ../cal/frs5.yamp.calPcal.R 	new file:   frs5-se2.analyse.R

---
+++ devel/frs5-se2.analyse.R
@@ -0,0 +1,37 @@
+library(R4CouchDB,
+        quietly = TRUE)
+
+
+cdb <- cdbIni()
+
+cdb$id     <- "7f696c493207f8d813f3b4e1f037207b"
+cdb$DBName <- "vaclab_db"
+
+## ----------------
+srcPath <- "../utils/"
+fn <- list.files(srcPath, pattern=".R$")
+for (k in 1:length(fn)) source(paste(srcPath,fn[k],sep=""))
+
+srcPath <- "../cal/"
+fn <- list.files(srcPath, pattern=".R$")
+for (k in 1:length(fn)) source(paste(srcPath,fn[k],sep=""))
+
+srcPath <- "../uncert/"
+fn <- list.files(srcPath, pattern=".R$")
+for (k in 1:length(fn)) source(paste(srcPath,fn[k],sep=""))
+## ----------------
+
+ccc <- cdbGetDoc(cdb)$res
+
+if(length(ccc$Calibration) > 0){
+
+  ccc <- refreshAnalysis(cdb,ccc)
+
+  ccc <- frs5.calPcal(ccc)
+#  ccc <- frs5.uncertPfrs5(ccc)
+
+
+  ##  cdb$dataList <- ccc
+
+  ##  res <- cdbAddDoc(cdb)$res
+}
